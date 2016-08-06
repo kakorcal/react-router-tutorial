9d1702d01e519fb2d5123b14d452391d925efb54
Kenneth Korcal <kakorcal@gmail.com>
2016-08-06 05:11:19 -0700

    convert to es6 class syntax

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,18 +1,41 @@
-import React from 'react'
+import React, {Component} from 'react'
 
-export default React.createClass({
-  getInitialState(){
-    return {counter: 0};
-  },
+// export default React.createClass({
+//   getInitialState(){
+//     return {counter: 0};
+//   },
+//   increment(){
+//     this.setState({counter: ++this.state.counter});
+//   },
+//   render() {
+//     return (
+//       <div>
+//         <div>Hello, React Router! {this.state.counter}</div>
+//         <button onClick={this.increment}>Increment</button>
+//       </div>
+//     );
+//   }
+// });
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
   increment(){
+    console.log(this);
     this.setState({counter: ++this.state.counter});
-  },
-  render() {
+  }
+  render(){
     return (
       <div>
-        <div>Hello, React Router! {this.state.counter}</div>
-        <button onClick={this.increment}>Increment</button>
+        <div>Hello, React Route! {this.state.counter}</div>
+        <button onClick={this.increment.bind(this)}>Increment</button>
       </div>
     );
   }
-})
+}
+
+export default App;
