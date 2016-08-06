2b9b7504bbeb7249bfe2cf30f85978a596fc03e5
Kenneth Korcal <kakorcal@gmail.com>
2016-08-06 04:01:02 -0700

    init commit

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,20 @@
 import React from 'react'
 
 export default React.createClass({
+  getInitialState(){
+    return {
+      counter: 0
+    }
+  },
+  increment(){
+    this.setState({counter: ++this.state.counter});
+  },
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+      <div>
+        <div>Hello, React Router! {this.state.counter}</div>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    );
   }
 })
--- lessons/01-setting-up/webpack.config.js
+++ lessons/01-setting-up/webpack.config.js
@@ -8,7 +8,14 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
     ]
   }
 }
